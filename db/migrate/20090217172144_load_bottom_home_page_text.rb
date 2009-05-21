12a36bcab4b446f5b575e956439d83ca0a7ddc85
Griff Wigley <griffinjay@griff-wigleys-imac.local>
2009-05-20 20:19:46 -0500

    Lots of text changes to the About Page.
    Some changes to the Home page.
    
    Some changes to the Contact page.
    
    A global find/replace on Spot.us/CSJN.
    
    A change to the footer (elevator pitch)

--- db/migrate/20090217172144_load_bottom_home_page_text.rb
+++ db/migrate/20090217172144_load_bottom_home_page_text.rb
@@ -1,6 +1,6 @@
 class LoadBottomHomePageText < ActiveRecord::Migration
   def self.up
-    SiteOption.create!(:key => :bottom_home_page_text, :value => "<h5 style=\"margin: 0pt;\">See our FAQ's for <a href=\"/pages/about#faq\"> Concerned Citizens, Reporters and News Organizations</a>. Spot.Us is a marketplace where all three come to collaborate.<p></p><p></p>Check out our <a href=\"http://blog.spot.us\">blog</a> join our <a href=\"http://www.facebook.com/home.php#/group.php?sid=b04b3406bd9281f4fe4df6c39efef5d5&amp;refurl=http%3A%2F%2Fwww.facebook.com%2Fs.php%3Fref%3Dsearch%26init%3Dq%26q%3Dspot.us%26sid%3Db04b3406bd9281f4fe4df6c39efef5d5&amp;gid=29554944739\">Facebook</a> or follow us on <a href=\"http://twitter.com/spotus\">Twitter</a>.</h5>")
+    SiteOption.create!(:key => :bottom_home_page_text, :value => "<h5 style=\"margin: 0pt;\">See our FAQ's for <a href=\"/pages/about#faq\"> Concerned Citizens, Reporters and News Organizations</a>. Community Supported Journalism, Northfield (CSJN) is a marketplace where all three come to collaborate.<p></p><p></p>Check out our <a href=\"http://blog.spot.us\">blog</a> join our <a href=\"http://www.facebook.com/home.php#/group.php?sid=b04b3406bd9281f4fe4df6c39efef5d5&amp;refurl=http%3A%2F%2Fwww.facebook.com%2Fs.php%3Fref%3Dsearch%26init%3Dq%26q%3Dspot.us%26sid%3Db04b3406bd9281f4fe4df6c39efef5d5&amp;gid=29554944739\">Facebook</a> or follow us on <a href=\"http://twitter.com/spotus\">Twitter</a>.</h5>")
   end
 
   def self.down
